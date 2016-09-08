217fad26ef26b8714ad6c1c5486b2d8a312c1e5b
RaysChen <543114160@qq.com>
2016-09-08 11:03:00 +0800

    About-ViewController
    自定义控制器切换，实现tab在上面的切换效果

---
+++ 自定义控制器的切换/自定义控制器的切换/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自定义控制器的切换
+//
+//  Created by 陈曦 on 16/9/8.
+//  Copyright © 2016年 chenxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 自定义控制器的切换/自定义控制器的切换/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  自定义控制器的切换
+//
+//  Created by 陈曦 on 16/9/8.
+//  Copyright © 2016年 chenxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
