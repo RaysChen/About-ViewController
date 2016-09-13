fdbc2458c06f493ab5acf1714c3b443bb87884e2
RaysChen <543114160@qq.com>
2016-09-13 15:09:16 +0800

    About-ViewController
    让级联菜单的两个tableview共用一个数据源

---
+++ 两个tableview共用一个数据源/两个tableview共用一个数据源/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  两个tableview共用一个数据源
+//
+//  Created by 陈曦 on 16/9/13.
+//  Copyright © 2016年 chenxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 两个tableview共用一个数据源/两个tableview共用一个数据源Tests/__tableview_______Tests.m
@@ -0,0 +1,39 @@
+//
+//  __tableview_______Tests.m
+//  两个tableview共用一个数据源Tests
+//
+//  Created by 陈曦 on 16/9/13.
+//  Copyright © 2016年 chenxi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __tableview_______Tests : XCTestCase
+
+@end
+
+@implementation __tableview_______Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
