583f3046101cd083b9ea6041428e0bcdbf6ba9bd
RaysChen <543114160@qq.com>
2016-09-14 19:09:37 +0800

    About-ViewController
    懒加载数据数组
    添加了一些资源文件

--- 两个tableview共用一个数据源/两个tableview共用一个数据源/ViewController.m
+++ 两个tableview共用一个数据源/两个tableview共用一个数据源/ViewController.m
@@ -7,12 +7,34 @@
 //
 
 #import "ViewController.h"
+#import "CXCategory.h"
 
 @interface ViewController ()
+
+//左边表格
+@property (weak, nonatomic) IBOutlet UITableView *category;
+//右边表格
+@property (weak, nonatomic) IBOutlet UITableView *subCategory;
+//所有类别数据
+@property (strong, nonatomic)NSArray *categories;
 
 @end
 
 @implementation ViewController
+
+- (NSArray *)categories{
+    
+    if (_categories == nil) {
+        NSArray *dictArray = [NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"categories" ofType:@"plist"]];
+        
+        NSMutableArray *categoryArray = [NSMutableArray array];
+        for (NSDictionary *dict in dictArray) {
+            [categoryArray addObject:[CXCategory categoryWithDict:dict]];
+        }
+        _categories = categoryArray;
+    }
+    return _categories;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
