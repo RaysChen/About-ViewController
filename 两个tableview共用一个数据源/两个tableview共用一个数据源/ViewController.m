020540502bc7bd852c8939f93dbe004375e1e292
RaysChen <543114160@qq.com>
2016-09-18 19:32:26 +0800

    About-ViewController

--- 两个tableview共用一个数据源/两个tableview共用一个数据源/ViewController.m
+++ 两个tableview共用一个数据源/两个tableview共用一个数据源/ViewController.m
@@ -9,12 +9,12 @@
 #import "ViewController.h"
 #import "CXCategory.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource,UITabBarDelegate>
 
 //左边表格
-@property (weak, nonatomic) IBOutlet UITableView *category;
+@property (weak, nonatomic) IBOutlet UITableView *categoryTableView;
 //右边表格
-@property (weak, nonatomic) IBOutlet UITableView *subCategory;
+@property (weak, nonatomic) IBOutlet UITableView *subCategoryTableView;
 //所有类别数据
 @property (strong, nonatomic)NSArray *categories;
 
@@ -38,12 +38,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // 默认选中左边表格的第0行
+    [self.categoryTableView selectRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0] animated:NO scrollPosition:UITableViewScrollPositionTop];
+    
+    self.subCategoryTableView.contentInset = UIEdgeInsetsMake(64, 0, 0, 0);
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+
 }
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    // 左边表格
+    if (tableView == self.categoryTableView) {
+        static NSString *ID = @"category";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+        
+        CXCategory *c = self.categories[indexPath.row];
+        
+        // 设置普通图片
+        cell.imageView.image = [UIImage imageNamed:c.icon];
+        // 设置高亮图片（cell选中 -> cell.imageView.highlighted = YES -> cell.imageView显示highlightedImage这个图片）
+        cell.imageView.highlightedImage = [UIImage imageNamed:c.highlighted_icon];
+        
+        // 设置label高亮时的文字颜色
+        cell.textLabel.highlightedTextColor = [UIColor redColor];
+        
+        cell.textLabel.text = c.name;
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        
+        return cell;
+    } else {
+        // 右边表格
+        static NSString *ID = @"subcategory";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+        
+        // 获得左边表格被选中的模型
+        CXCategory *c = self.categories[self.categoryTableView.indexPathForSelectedRow.row];
+        cell.textLabel.text = c.subcategories[indexPath.row];
+        
+        return cell;
+    }
+}
+
+#pragma mark - <UITableViewDelegate>
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (tableView == self.categoryTableView) {
+        [self.subCategoryTableView reloadData];
+    }
+}
+
 
 @end
