9b59b1125c7889a824fdc40ded2a86d6211328aa
RaysChen <543114160@qq.com>
2016-09-19 18:28:28 +0800

    About-ViewController

---
+++ 模仿网易新闻首页/模仿网易新闻首页/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  模仿网易新闻首页
+//
+//  Created by 陈曦 on 16/9/19.
+//  Copyright © 2016年 chenxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 模仿网易新闻首页/模仿网易新闻首页/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  模仿网易新闻首页
+//
+//  Created by 陈曦 on 16/9/19.
+//  Copyright © 2016年 chenxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
